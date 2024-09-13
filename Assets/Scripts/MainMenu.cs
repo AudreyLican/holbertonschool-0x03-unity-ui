118e83705ce94cb40da720b32506217b88082941
AudreyLican <audrey.lican@gmail.com>
2024-09-13 09:11:37 -0400

    added colorblindness mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,25 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
-{/*
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
+{
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
 
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
     }
 
     public void QuitMaze()
